c490bf31fbeccee53913d03e904fbbca68623423
javierarancibia <69688629+javierarancibia@users.noreply.github.com>
2022-12-14 17:38:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Non-Case-sensitive-fn-to-return-strings-from-array-by-x-cases
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-6kmjr1)
---
+++ index.js
@@ -0,0 +1,5 @@
+const arr = ['Welcome', 'to', 'The', 'world'];
+
+console.log(arr.filter((str) => str.toLowerCase().startsWith('t')));
+
+// function to return strings in array that start with x number of cases without case sensitivity
